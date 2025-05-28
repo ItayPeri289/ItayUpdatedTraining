50a6ae6cebc4b0fe04a454b61f17148319b6b3d9
ItayPeri289 <peri.itay289@gmail.com>
2025-05-28 11:24:27 +0300

    removed class and css file

--- FrontEndTraining/src/components/HomePage/HomePage.tsx
+++ FrontEndTraining/src/components/HomePage/HomePage.tsx
@@ -1,9 +1,16 @@
 import productsArray from "../../data/Products.json";
 import ProductCard from "./ProductCard";
-import "./HomePage.css";
 
 const HomePage = () => (
-  <div className="items">
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "row-reverse",
+      justifyContent: "center",
+      flexWrap: "wrap",
+      alignItems: "flex-start",
+      gap: "0.7rem",
+    }}>
     {productsArray.map((item) => (
       <ProductCard
         imageUrl={item.image}
